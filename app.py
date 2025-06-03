8232ec75134d4c13d5aa9d269c0012d17b2f2d75
Rahul Patel <115304614+Rahulpate93016@users.noreply.github.com>
2025-06-03 09:46:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -75,5 +75,5 @@ years_exp = st.number_input("Enter Years of Experience:", min_value=0.0, max_val
 if st.button("Predict Salary"):
     input_data = np.array([[years_exp]])
     prediction = model.predict(input_data)
-st.success(f"For {years_exp} years of experience, your predicted monthly salary is ₹{prediction[0]:,.2f}")
+st.success(f"For {years_exp} years of experience, your predicted monthly salary is ₹{prediction:,.2f}")
 
